9c139deebdc16562cbe3ce6d3715407664c797ca
VLFanai <89696427+VLFanai@users.noreply.github.com>
2024-02-28 21:55:59 +0530

    adding more operators

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-num1 = input("Enter a number: ")
-op = input("Enter operator (+, -, *, /): ")
-num2 = input("Enter another number: ")
-
-num1 = float(num1)
-num2 = float(num2)
-
-if op == '+':
-    result = num1 + num2
-    print(f"The sum of {num1:.0f} and {num2:.0f} is: {result}")
-elif op == '/':
-    result = num1 / num2
-    print(f"The sum of {num1:.0f} and {num2:.0f} is: {result}")
-elif op == '*':
-    result = num1 * num2
-    print(f"The sum of {num1:.0f} and {num2:.0f} is: {result}")
-elif op == '-':
-    result = num1 - num2
-    print(f"The sum of {num1:.0f} and {num2:.0f} is: {result}")
-else:
-    print("Invalid Operator")
-
+print("Hello VL")
 
 
