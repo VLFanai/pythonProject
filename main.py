2745933e6dcea1c8b5884b55733c52651e9d015c
VLFanai <89696427+VLFanai@users.noreply.github.com>
2024-03-02 21:39:19 +0530

    adding more operators

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ root.title("Notes App")
 root.geometry("500x500")
 style = Style(theme='journal')
 style = ttk.Style()
+
+#Configurate the tab font to be bold
+style.configure("TNotebook.Tab", font=("TkDefaultFont", 14, "bold"))
+
+notebook = ttk.Notebook(root, style="TNotebook")
+
+notes = {}
+try:
+    with open("otes.json", "r") as f:
+        notes = json.load(f)
+except FileNotFoundError:
+    pass
+
+notebook = ttk.Notebook(root)
+notebook.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+def add_note():
