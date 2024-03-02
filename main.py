a3b9a2fa8525f2c6565c21abb83dc207efff0fd4
VLFanai <89696427+VLFanai@users.noreply.github.com>
2024-03-02 21:34:42 +0530

    adding more operators

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from tkinter import ttk, messagebox
 import json
 from ttkbootstrap import Style
 
-root = tk
+root = tk.Tk()
+root.title("Notes App")
+root.geometry("500x500")
+style = Style(theme='journal')
+style = ttk.Style()
